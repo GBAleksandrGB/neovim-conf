d187b97c262786dcbc4b5b457939bbe8e437a81c
GBAleksandrGB <tomilov.alex@gmail.com>
2025-04-10 20:19:52 +0300

    fix: lazy, lua_ls, neo-tree diagnostic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,37 +2,40 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require('lspconfig')
-		--	lspconfig.pyright.setup({})
+			local lspconfig = require("lspconfig")
 			lspconfig.ruff.setup({})
 			lspconfig.lua_ls.setup({
 				on_init = function(client)
 					if client.workspace_folders then
 						local path = client.workspace_folders[1].name
-						if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+						if
+							path ~= vim.fn.stdpath("config")
+							---@diagnostic disable-next-line: undefined-field
+							and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
+						then
 							return
 						end
 					end
 
-					client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+					client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 						runtime = {
 							-- Tell the language server which version of Lua you're using
 							-- (most likely LuaJIT in the case of Neovim)
-							version = 'LuaJIT'
+							version = "LuaJIT",
 						},
 						workspace = {
 							checkThirdParty = false,
 							library = {
-								vim.env.VIMRUNTIME
-							}
+								vim.env.VIMRUNTIME,
+							},
 							-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
 							-- library = vim.api.nvim_get_runtime_file("", true)
-						}
+						},
 					})
 				end,
 				settings = {
-					Lua = {}
-				}
+					Lua = {},
+				},
 			})
 
 			vim.api.nvim_create_autocmd("LspAttach", {
@@ -53,6 +56,6 @@ return {
 					end, opts)
 				end,
 			})
-		end
-	}
+		end,
+	},
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,25 +7,25 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none"})
-		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none"})
-		--vim.diagnostic.config({
-		--  signs = {
-		--		text = {
-		--			[vim.diagnostic.severity.ERROR] = " ",
-		--			[vim.diagnostic.severity.WARN] = " ",
-		--			[vim.diagnostic.severity.INFO] = " ",
-		--			[vim.diagnostic.severity.HINT] = " ",
-		--		},
-		--	},
-		--})
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none" })
+		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = "none" })
+		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = " ",
+					[vim.diagnostic.severity.WARN] = " ",
+					[vim.diagnostic.severity.INFO] = " ",
+					[vim.diagnostic.severity.HINT] = " ",
+				},
+			},
+		})
 		-- In older versions, you can define the signs manually:
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+		--vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+		--vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+		--vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+		--vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 		require("neo-tree").setup({
 			close_if_last_window = false,
 		})
-	end
+	end,
 }
